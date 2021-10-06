2c2b3a2b5d2f8191424034aa92a5d6301ab6fecf
NaikAayush <ayush@pesu.pes.edu>
2021-10-07 00:49:59 +0530

    feat: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: "",
+  purge: {
+    enable: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
