196bb0819ec541f9f737b97f701bbb7bc6c95457
NaikAayush <ayush@pesu.pes.edu>
2021-10-07 01:21:28 +0530

    feat: login ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
   },
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "login-illustration": "url('assets/login/login-illustration.svg')",
+      },
+      colors: {
+        custom: {
+          100: "#E5EBF8",
+          200: "#343434",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
