6791ca98e86a8c3de8e9d8f6e64006e6479cd0db
NaikAayush <ayush@pesu.pes.edu>
2021-10-08 14:31:52 +0530

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { SkillsAddComponent } from './components/skills/skills-add/skills-add.component';
+import { SkillsListComponent } from './components/skills/skills-list/skills-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'skills', component: SkillsListComponent },
+  { path: 'skills/add', component: SkillsAddComponent },
 ];
 
 @NgModule({
